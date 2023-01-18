398a6e76ceecfe8408bbc028928b76b67e48214d
jacko2611 <jacksonkent70@gmail.com>
2023-01-18 21:40:24 +1100

    added questions to index.js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,50 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub user name?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Please write a detailed description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please write any installation insctructions necessary',
+        name: 'instructions'
+    },
+    {
+        type: 'input',
+        message: 'Any usage information the user should know about?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'List any collaborators, along with their Github profiles and any sources that need attribution',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Please detail any testing the user may perfom?',
+        name: 'testing'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+]
 
 
 // TODO: Create a function to write README file
